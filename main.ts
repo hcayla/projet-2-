2c06a1d2993409b547e78e9ce7fbac84625f4cd5
hcayla <128042961+hcayla@users.noreply.github.com>
2023-03-16 14:19:05 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 A B G A F G E ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    led.plot(1, 2)
+})
+basic.forever(function () {
+	
+})
